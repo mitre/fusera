d26a35b842df7077e98f5609328829419694ea9a
Matt <mattrbianchi@gmail.com>
2018-06-25 10:21:15 -0400

    Install script supports mac and linux

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!bin/bash
 
-curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.8/fusera-linux-amd64
+OS=linux
+if [ "$(uname)" == "Darwin" ]; then
+	OS="darwin"
+fi
+
+curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.8/fusera-${OS}-amd64
 
 chmod +x /usr/local/bin/fusera
