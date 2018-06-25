36e210d1215e35b0cf4669dd5c41afb8ffd8ee9a
Matt <mattrbianchi@gmail.com>
2018-06-25 12:03:48 -0400

    install sracp in install script

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.8/fusera-${OS}-amd64
+curl -s -L -o /usr/local/bin/sracp https://github.com/mitre/fusera/releases/download/v0.0.8/sracp-${OS}-amd64
 
 chmod +x /usr/local/bin/fusera
+chmod +x /usr/local/bin/sracp
