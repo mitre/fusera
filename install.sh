731d00c0b1f65e50462db9e0ef5bf6cf144a37cd
Matt Bianchi <mattrbianchi@users.noreply.github.com>
2018-07-05 13:53:51 -0400

    Update to reflect new build process

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!bin/bash
 
-OS=linux
-if [ "$(uname)" == "Darwin" ]; then
-	OS="darwin"
-fi
-
-curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.8/fusera-${OS}-amd64
-curl -s -L -o /usr/local/bin/sracp https://github.com/mitre/fusera/releases/download/v0.0.8/sracp-${OS}-amd64
+curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.9/fusera
+curl -s -L -o /usr/local/bin/sracp https://github.com/mitre/fusera/releases/download/v0.0.9/sracp
 
 chmod +x /usr/local/bin/fusera
 chmod +x /usr/local/bin/sracp
