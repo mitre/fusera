4b9d68233619bde5beaac4b55be56ee42b39a297
Matt <mattrbianchi@gmail.com>
2018-07-17 23:40:24 -0400

    bump install script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 
-curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.9/fusera
-curl -s -L -o /usr/local/bin/sracp https://github.com/mitre/fusera/releases/download/v0.0.9/sracp
+curl -s -L -o /usr/local/bin/fusera
+https://github.com/mitre/fusera/releases/download/v0.0.10/fusera
+curl -s -L -o /usr/local/bin/sracp
+https://github.com/mitre/fusera/releases/download/v0.0.10/sracp
 
 chmod +x /usr/local/bin/fusera
 chmod +x /usr/local/bin/sracp
