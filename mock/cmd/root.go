4e2f42b4ec70f30135dc278e200d45f94dadc50a
Matt <mattrbianchi@gmail.com>
2018-07-29 17:09:30 -0400

    Add mock sdl api app for testing purposes

---
+++ mock/cmd/root.go
@@ -0,0 +1,102 @@
+// Modifications Copyright 2018 The MITRE Corporation
+// Authors: Matthew Bianchi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	debug bool
+)
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "Enable debug output.")
+	if err := viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup("debug")); err != nil {
+		panic("INTERNAL ERROR: could not bind debug flag to debug environment variable")
+	}
+
+	viper.AutomaticEnv()
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "mocksdlapi",
+	Short: "A mock implementation of the SDL API.",
+	Long:  ``,
+	RunE:  run,
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	// Start up an http server and serve 5019 accessions named an+1 with n starting at 0
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", r)
+	return nil
+}
+
+type payload struct {
+	ID      string `json:"accession,omitempty"`
+	Status  int    `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
+	Files   []file `json:"files,omitempty"`
+}
+
+type file struct {
+	Name           string    `json:"name,omitempty"`
+	Size           string    `json:"size,omitempty"`
+	Type           string    `json:"type,omitempty"`
+	ModifiedDate   time.Time `json:"modificationDate,omitempty"`
+	Md5Hash        string    `json:"md5,omitempty"`
+	Link           string    `json:"link,omitempty"`
+	ExpirationDate time.Time `json:"expirationDate,omitempty"`
+	Service        string    `json:"service,omitempty"`
+}
+
+// HomeHandler returns whatever JSON I want.
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	response := make([]payload, 5045, 5045)
+	for i := range response {
+		response[i].ID = "a" + fmt.Sprintf("%d", i)
+		response[i].Status = 200
+		response[i].Files = make([]file, 3, 3)
+		for j := range response[i].Files {
+			response[i].Files[j].Name = "a" + fmt.Sprintf("%d", i) + "file" + fmt.Sprintf("%d", j)
+			response[i].Files[j].ExpirationDate = time.Now().Add(time.Hour)
+		}
+	}
+	if err := json.NewEncoder(w).Encode(&response); err != nil {
+		panic("couldn't encode json")
+	}
+	fmt.Println(response)
+}
+
+// Execute runs the root command of mocksdlapi.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
