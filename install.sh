146f08b6228defa7c54377f8841118997bb45439
Matt <mattrbianchi@gmail.com>
2018-09-10 10:17:25 -0400

    version .13

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.12/fusera
-curl -s -L -o /usr/local/bin/sracp https://github.com/mitre/fusera/releases/download/v0.0.12/sracp
+curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.13/fusera
+curl -s -L -o /usr/local/bin/sracp https://github.com/mitre/fusera/releases/download/v0.0.13/sracp
 
 chmod +x /usr/local/bin/fusera
 chmod +x /usr/local/bin/sracp
