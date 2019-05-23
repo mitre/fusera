eff38015a66987040bac54e0b8e925af174975eb
Matt Bianchi <mattrbianchi@users.noreply.github.com>
2019-05-23 13:52:50 -0400

    bump install to .18

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.16/fusera
-curl -s -L -o /usr/local/bin/sracp https://github.com/mitre/fusera/releases/download/v0.0.16/sracp
+curl -s -L -o /usr/local/bin/fusera https://github.com/mitre/fusera/releases/download/v0.0.18/fusera
+curl -s -L -o /usr/local/bin/sracp https://github.com/mitre/fusera/releases/download/v0.0.18/sracp
 
 chmod +x /usr/local/bin/fusera
 chmod +x /usr/local/bin/sracp
